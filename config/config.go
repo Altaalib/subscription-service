3f66d57d602b0b3c97b69736048d5242f3465856
Altaalib <ismayil.mukhtadov@gmail.com>
2026-03-01 14:51:32 +0300

    feat: add config

---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Database DatabaseConfig
+}
+
+type ServerConfig struct {
+	Port string
+}
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		Server: ServerConfig{
+			Port: viper.GetString("SERVER_PORT"),
+		},
+		Database: DatabaseConfig{
+			Host:     viper.GetString("DB_HOST"),
+			Port:     viper.GetString("DB_PORT"),
+			User:     viper.GetString("DB_USER"),
+			Password: viper.GetString("DB_PASSWORD"),
+			DBName:   viper.GetString("DB_NAME"),
+			SSLMode:  viper.GetString("DB_SSLMODE"),
+		},
+	}, nil
+}
+
+func (d *DatabaseConfig) DSN() string {
+	return "host=" + d.Host +
+		" port=" + d.Port +
+		" user=" + d.User +
+		" password=" + d.Password +
+		" dbname=" + d.DBName +
+		" sslmode=" + d.SSLMode
+}
